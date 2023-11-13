400e3de0ef9f7c9823bca1951f029d97551353ac
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-13 20:31:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVijezba05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napišite program koji će zatražiti od korisnika da unese neku rečenicu te će ispisati prvu i zadnju riječ te rečenice.
+            Console.WriteLine("Upišite nekakvu rečenicu: ");
+            string tekst = Console.ReadLine();
+            string[] rijeci = tekst.Split(' ');
+            if(rijeci.Length >= 2 )
+            {
+                Console.WriteLine("prva rijec je: " + rijeci[0]);
+                Console.WriteLine("zadnja rijec je: " + rijeci[rijeci.Length - 1]);
+
+            }
+            else
+            {
+                Console.WriteLine("Upišite barem dvije rijeci.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
